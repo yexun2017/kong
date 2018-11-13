a7b5ea2503443c0fd1cf01031ceb9f518593d383
Aapo Talvensaari <aapo.talvensaari@gmail.com>
2018-11-13 15:46:38 +0200

    chore(response-transformer) make plugin defensive against possible errors between the phases

--- kong/plugins/response-transformer/handler.lua
+++ kong/plugins/response-transformer/handler.lua
@@ -1,10 +1,14 @@
 local BasePlugin = require "kong.plugins.base_plugin"
-local body_filter = require "kong.plugins.response-transformer.body_transformer"
-local header_filter = require "kong.plugins.response-transformer.header_transformer"
+local body_transformer = require "kong.plugins.response-transformer.body_transformer"
+local header_transformer = require "kong.plugins.response-transformer.header_transformer"
 
-local is_body_transform_set = header_filter.is_body_transform_set
-local is_json_body = header_filter.is_json_body
-local table_concat = table.concat
+
+local is_body_transform_set = header_transformer.is_body_transform_set
+local is_json_body = header_transformer.is_json_body
+local concat = table.concat
+local kong = kong
+local ngx = ngx
+
 
 local ResponseTransformerHandler = BasePlugin:extend()
 
@@ -13,33 +17,46 @@ function ResponseTransformerHandler:new()
   ResponseTransformerHandler.super.new(self, "response-transformer")
 end
 
+
 function ResponseTransformerHandler:header_filter(conf)
   ResponseTransformerHandler.super.header_filter(self)
-  header_filter.transform_headers(conf, ngx.header)
+
+  if kong.response.get_source() ~= "service" then
+    return
+  end
+
+  header_transformer.transform_headers(conf)
 end
+
 
 function ResponseTransformerHandler:body_filter(conf)
   ResponseTransformerHandler.super.body_filter(self)
 
-  if is_body_transform_set(conf) and is_json_body(ngx.header["content-type"]) then
-    local ctx = ngx.ctx
-    local chunk, eof = ngx.arg[1], ngx.arg[2]
+  if kong.response.get_source() ~= "service" or
+     not (is_body_transform_set(conf) and
+          is_json_body(kong.request.get_header("Content-Type"))) then
+    return
+  end
 
-    ctx.rt_body_chunks = ctx.rt_body_chunks or {}
-    ctx.rt_body_chunk_number = ctx.rt_body_chunk_number or 1
+  local ctx = ngx.ctx
+  local chunk, eof = ngx.arg[1], ngx.arg[2]
 
-    if eof then
-      local body = body_filter.transform_json_body(conf, table_concat(ctx.rt_body_chunks))
-      ngx.arg[1] = body
-    else
-      ctx.rt_body_chunks[ctx.rt_body_chunk_number] = chunk
-      ctx.rt_body_chunk_number = ctx.rt_body_chunk_number + 1
-      ngx.arg[1] = nil
-    end
+  ctx.rt_body_chunks = ctx.rt_body_chunks or {}
+  ctx.rt_body_chunk_number = ctx.rt_body_chunk_number or 1
+
+  if eof then
+    local body = body_transformer.transform_json_body(conf, concat(ctx.rt_body_chunks))
+    ngx.arg[1] = body
+  else
+    ctx.rt_body_chunks[ctx.rt_body_chunk_number] = chunk
+    ctx.rt_body_chunk_number = ctx.rt_body_chunk_number + 1
+    ngx.arg[1] = nil
   end
 end
 
+
 ResponseTransformerHandler.PRIORITY = 800
-ResponseTransformerHandler.VERSION = "0.1.1"
+ResponseTransformerHandler.VERSION = "1.0.0"
+
 
 return ResponseTransformerHandler
